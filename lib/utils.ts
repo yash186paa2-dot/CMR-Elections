596d765f29c209dbe3e974734f1fea0b8ad3b57c
yash186paa2-dot <yash186paa2@gmail.com>
2026-06-15 18:05:25 +0530

    fix: export 'cn' utility function from lib/utils.ts to resolve build error

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 /**
  * Normalizes election status values by removing extra quotes, 
  * trimming whitespace, and converting to lowercase.
